fec2db1f96757f7bd8b5a021ee32de88cae7c42c
Andreas Blattmann <andreas.blattmann@iwr.uni-heidelberg.de>
2021-02-26 20:35:33 +0100

    add working pose estimator for considered datasets

--- lib/dataset/infer_datasets.py
+++ lib/dataset/infer_datasets.py
@@ -1,13 +1,67 @@
+import os
 from os import path
 import pickle
 import numpy as np
 import torch
+from torchvision import transforms as T
 from torch.utils.data import Dataset
+import cv2
+
 
 class InferenceDataset(Dataset):
 
     def __init__(self,dataset):
         super().__init__()
         self.dataset = dataset
-        assert self.dataset in ['iper','h36m']
+        self.transforms = T.Compose([T.ToTensor(),
+                                     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        assert self.dataset in ['iper','h36m','taichi']
 
+        if self.dataset == 'iper':
+            self.metafile = '/export/scratch/compvis/datasets/iPER/processed_256_resized/iper_256_resized_frange.p'
+            self.datapath = '/export/scratch/compvis/datasets/iPER/processed_256_resized'
+        elif self.dataset == 'taichi':
+            self.metafile = '/export/scratch/compvis/datasets/taichi/taichi/meta_with_10_20.p'
+            self.datapath = '/export/scratch/compvis/datasets/taichi/taichi/'
+        else:
+            self.metafile = "/export/scratch/compvis/datasets/human3.6M/video_prediction/h36m_test_smaller.p"
+            self.datapath = '/export/scratch/compvis/datasets/human3.6M/video_prediction'
+
+        if 'DATAPATH' in os.environ:
+            self.metafile = path.join(os.environ['DATAPATH'],self.metafile[1:])
+            self.datapath = path.join(os.environ['DATAPATH'],self.datapath[1:])
+        with open(self.metafile,'rb') as f:
+            self.data = pickle.load(f)
+
+        self.data["img_path"] = [
+            path.join(self.datapath, p if not p.startswith("/") else p[1:]) for p in self.data["img_path"]
+        ]
+
+        self.data = {key: np.asarray(self.data[key]) for key in self.data}
+
+        self.data.update({'did':np.arange(self.data['img_path'].shape[0])})
+
+
+
+
+    def __len__(self):
+        return self.data['img_path'].shape[0]
+
+    def __getitem__(self, id):
+
+        return {'id' : self._get_id(id),
+                'img' : self._get_img(id)}
+
+    def _get_id(self,idx):
+        return self.data['did'][idx]
+
+    def _get_img(self,idx):
+
+        img_path = self.data['img_path'][idx]
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img,dsize=(256,256),interpolation=cv2.INTER_LINEAR)
+
+
+        img = self.transforms(img)
+        return img
--- tools/infer.py
+++ tools/infer.py
@@ -19,14 +19,25 @@ import torch.backends.cudnn as cudnn
 import torch.optim
 import torch.utils.data
 import torch.utils.data.distributed
+import torchvision
 import torchvision.transforms as transforms
+import numpy as np
+import cv2
+import time
+import math
+from tqdm import tqdm
 
 import _init_paths
 from config import cfg
 from config import update_config
-from core.loss import JointsMSELoss
-from core.function import validate
+from core.inference import get_final_preds,get_max_preds
+from utils.transforms import flip_back
+from core.evaluate import accuracy
+
+
+
 from utils.utils import create_logger
+from dataset.infer_datasets import InferenceDataset
 
 import dataset
 import models
@@ -53,17 +64,165 @@ def parse_args():
                         help='log directory',
                         type=str,
                         default='')
-    parser.add_argument('--dataDir',
-                        help='data directory',
-                        type=str,
-                        default='')
     parser.add_argument('--prevModelDir',
                         help='prev Model directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--dataDir',
+                        help='data directory',
                         type=str,
                         default='')
 
     args = parser.parse_args()
     return args
+
+
+def save_batch_image_with_joints(batch_image, batch_joints, batch_joints_vis,
+                                 file_name, nrow=8, padding=2):
+    '''
+    batch_image: [batch_size, channel, height, width]
+    batch_joints: [batch_size, num_joints, 3],
+    batch_joints_vis: [batch_size, num_joints, 1],
+    }
+    '''
+    grid = torchvision.utils.make_grid(batch_image, nrow, padding, True)
+    ndarr = grid.mul(255).clamp(0, 255).byte().permute(1, 2, 0).cpu().numpy()
+    ndarr = ndarr.copy()
+
+    nmaps = batch_image.size(0)
+    xmaps = min(nrow, nmaps)
+    ymaps = int(math.ceil(float(nmaps) / xmaps))
+    height = int(batch_image.size(2) + padding)
+    width = int(batch_image.size(3) + padding)
+    k = 0
+    for y in range(ymaps):
+        for x in range(xmaps):
+            if k >= nmaps:
+                break
+            joints = batch_joints[k]
+            #joints_vis = batch_joints_vis[k]
+
+            for joint, joint_vis in zip(joints, joints):
+                joint[0] = x * width + padding + joint[0]
+                joint[1] = y * height + padding + joint[1]
+                # if joint_vis[0]:
+
+                cv2.circle(ndarr, (int(joint[0]), int(joint[1])), 2, [255, 0, 0], 2)
+            k = k + 1
+    ndarr = cv2.cvtColor(ndarr,cv2.COLOR_RGB2BGR)
+    cv2.imwrite(file_name, ndarr)
+
+
+def save_debug_images(config, input,# meta,
+                      joints_pred,
+                      prefix):
+    if not config.DEBUG.DEBUG:
+        return
+
+
+    if config.DEBUG.SAVE_BATCH_IMAGES_PRED:
+        save_batch_image_with_joints(
+            input, joints_pred, [],
+            #meta['joints_vis'],
+            '{}_pred.jpg'.format(prefix)
+        )
+
+def infer(config, val_loader, val_dataset, model, output_dir,device,num_keypoints=16):
+
+    # switch to evaluate mode
+    model.eval()
+
+    num_samples = len(val_dataset)
+    all_preds = np.zeros(
+        (num_samples, config.MODEL.NUM_JOINTS, 3),
+        dtype=np.float32
+    )
+    all_boxes = np.zeros((num_samples, 6))
+    image_path = []
+    filenames = []
+    imgnums = []
+    idx = 0
+    data_dict = val_dataset.data.copy()
+    data_dict.update({'keypoints_abs': np.zeros((len(val_dataset),num_keypoints,2)),
+                      'keypoints_rel': np.zeros((len(val_dataset),num_keypoints,2))})
+    with torch.no_grad():
+        for i, batch in enumerate(tqdm(val_loader,desc=f'Estimating keypoints')):
+            # compute output
+            input = batch['img'].to(device)
+            ids = batch['id'].numpy()
+            outputs = model(input)
+            if isinstance(outputs, list):
+                output = outputs[-1]
+            else:
+                output = outputs
+
+            # if config.TEST.FLIP_TEST:
+            #     input_flipped = input.flip(3)
+            #     outputs_flipped = model(input_flipped)
+            #
+            #     if isinstance(outputs_flipped, list):
+            #         output_flipped = outputs_flipped[-1]
+            #     else:
+            #         output_flipped = outputs_flipped
+            #
+            #     output_flipped = flip_back(output_flipped.cpu().numpy(),
+            #                                val_dataset.flip_pairs)
+            #     output_flipped = torch.from_numpy(output_flipped.copy()).cuda()
+            #
+            #
+            #     # feature is not aligned, shift flipped heatmap for higher accuracy
+            #     if config.TEST.SHIFT_HEATMAP:
+            #         output_flipped[:, :, :, 1:] = \
+            #             output_flipped.clone()[:, :, :, 0:-1]
+            #
+            #     output = (output + output_flipped) * 0.5
+
+
+
+            num_images = input.size(0)
+            # # measure accuracy and record loss
+            # losses.update(loss.item(), num_images)
+            # _, avg_acc, cnt, pred = accuracy(output.cpu().numpy(),
+            #                                  target.cpu().numpy())
+
+            # multiply by four,to obtain resolution of 256x256
+            pred, _ = get_max_preds(output.cpu().numpy()) * 4
+
+            # acc.update(avg_acc, cnt)
+            #
+            # # measure elapsed time
+            # batch_time.update(time.time() - end)
+            # end = time.time()
+            #
+            # c = meta['center'].numpy()
+            # s = meta['scale'].numpy()
+            # score = meta['score'].numpy()
+
+            # preds, maxvals = get_final_preds(
+            #     config, output.clone().cpu().numpy(), c, s)
+            #
+            # all_preds[idx:idx + num_images, :, 0:2] = preds[:, :, 0:2]
+            # all_preds[idx:idx + num_images, :, 2:3] = maxvals
+            # double check this all_boxes parts
+            # all_boxes[idx:idx + num_images, 0:2] = c[:, 0:2]
+            # all_boxes[idx:idx + num_images, 2:4] = s[:, 0:2]
+            # all_boxes[idx:idx + num_images, 4] = np.prod(s*200, 1)
+            # all_boxes[idx:idx + num_images, 5] = score
+            # image_path.extend(meta['image'])
+
+            idx += num_images
+            data_dict['keypoints_abs'][ids] = pred
+            data_dict['keypoints_rel'][ids] = pred / 256.
+
+            if i % config.PRINT_FREQ == 0:
+                prefix = '{}_{}'.format(
+                    os.path.join(output_dir, 'val'), i
+                )
+                save_debug_images(config, input,
+                                  #meta,
+                                  pred,
+                                  prefix)
+
 
 
 def main():
@@ -95,35 +254,25 @@ def main():
         logger.info('=> loading model from {}'.format(model_state_file))
         model.load_state_dict(torch.load(model_state_file))
 
-    model = torch.nn.DataParallel(model, device_ids=cfg.GPUS).cuda()
+    device = torch.device(f'cuda:{cfg.GPUS}' if torch.cuda.is_available() else 'cpu')
 
-    # define loss function (criterion) and optimizer
-    criterion = JointsMSELoss(
-        use_target_weight=cfg.LOSS.USE_TARGET_WEIGHT
-    ).cuda()
+    model.to(device)
 
-    # Data loading code
-    normalize = transforms.Normalize(
-        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-    )
-    valid_dataset = eval('dataset.'+cfg.DATASET.DATASET)(
-        cfg, cfg.DATASET.ROOT, cfg.DATASET.TEST_SET, False,
-        transforms.Compose([
-            transforms.ToTensor(),
-            normalize,
-        ])
-    )
+    # # define loss function (criterion) and optimizer
+    # criterion = JointsMSELoss(
+    #     use_target_weight=cfg.LOSS.USE_TARGET_WEIGHT
+    # ).cuda()
+    valid_dataset = InferenceDataset(dataset=cfg.DATASET.DATASET)
     valid_loader = torch.utils.data.DataLoader(
         valid_dataset,
-        batch_size=cfg.TEST.BATCH_SIZE_PER_GPU*len(cfg.GPUS),
-        shuffle=False,
+        batch_size=cfg.TEST.BATCH_SIZE_PER_GPU,
+        shuffle=True,
         num_workers=cfg.WORKERS,
         pin_memory=True
     )
 
     # evaluate on validation set
-    validate(cfg, valid_loader, valid_dataset, model, criterion,
-             final_output_dir, tb_log_dir)
+    infer(cfg, valid_loader, valid_dataset, model,final_output_dir,device=device)
 
 
 if __name__ == '__main__':
